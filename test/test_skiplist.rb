c2478360a811afd19a50ae25ccde7b2c5d83fd42
thestonage <iconofthestoneage@gmail.com>
2010-07-14 19:06:04 +0200

    Improved UnitTests

--- test/test_skiplist.rb
+++ test/test_skiplist.rb
@@ -2,13 +2,27 @@ require "test/unit"
 require "lib/skiplist.rb"
 
 class TestSkipList < Test::Unit::TestCase
- def setup
-      @data = [3, 6, 7, 9, 12, 17, 19, 21, 25, 26]
-      @skiplist = SkipList.new(16)
-      @skiplist.insert(@data)
- end
+    def setup
+        @unsorted = [84, 52, 53, 35, 46, 18, 95, 88, 26, 4]
+        @sorted   = [4, 18, 26, 35, 46, 52, 53, 84, 88, 95]
+        @skiplist = SkipList.new(16)
+    end
 
- def test_to_a
-     assert_equal(@skiplist.to_a, @data, "Should be equal")
- end
+    def test_insert_array
+         @skiplist.insert(@unsorted)
+         assert_equal(@sorted, @skiplist.to_a, "Should be equal")
+    end
+
+    def test_insert_value
+        @skiplist.insert(@unsorted)
+        @skiplist.insert(10)
+        assert_equal((@unsorted << 10).sort, @skiplist.to_a, "Should be equal")
+    end
+
+    def test_search
+        @skiplist.insert(@unsorted)
+        assert_equal(52, @skiplist.search(52), "Should return 52 as 52 is a skiplist member")
+        assert_equal(nil, @skiplist.search(54), "Should return nil as 54 is not a skiplist member")
+    end
+
 end
