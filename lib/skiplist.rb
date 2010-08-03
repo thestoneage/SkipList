0b7695b97e6f94ddbd998e8d3a0f07a3b3766535
Jörg W Mittag <JoergWMittag+Ruby@GoogleMail.Com>
2010-08-03 12:23:08 +0200

    Reformat to standard Ruby style. Note: no behavioral changes in this commit, only whitespace.

--- lib/skiplist.rb
+++ lib/skiplist.rb
@@ -1,138 +1,137 @@
 class SkipListNode
-    include Comparable
-    attr_reader :value, :forward
+  include Comparable
+  attr_reader :value, :forward
 
-    def initialize(value, forward)
-        @value, @forward = value, forward
-    end
+  def initialize(value, forward)
+    @value, @forward = value, forward
+  end
 
-    def <=>(other)
-        @value <=> other.value
-    end
+  def <=>(other)
+    @value <=> other.value
+  end
 
-    def next
-        @forward[0]
-    end
+  def next
+    @forward[0]
+  end
 end
 
 class << (Infinity = Object.new)
-    include Comparable
+  include Comparable
 
-    def <=> _
-        return 1
-    end
+  def <=> _
+    return 1
+  end
 
-    def to_s
-        'E'
-    end
+  def to_s
+    'E'
+  end
 end
 
 class SkipList
-    include Enumerable
-    def initialize(levels)
-        @last  = SkipListNode.new(Infinity, [])
-        @head = SkipListNode.new('H', [@last]*levels)
-    end
+  include Enumerable
+  def initialize(levels)
+    @last = SkipListNode.new(Infinity, [])
+    @head = SkipListNode.new('H', [@last]*levels)
+  end
 
-    def max_level
-        @head.forward.size - 1
-    end
+  def max_level
+    @head.forward.size - 1
+  end
 
-    def << value
-        new_node = SkipListNode.new(value, [])
-        search_path = node_search_path value
-        search_path.reverse[0, random_width].each_with_index do |source, level|
-            new_node.forward[level] = source.forward[level]
-            source.forward[level] = new_node
-        end
+  def << value
+    new_node = SkipListNode.new(value, [])
+    search_path = node_search_path value
+    search_path.reverse[0, random_width].each_with_index do |source, level|
+      new_node.forward[level] = source.forward[level]
+      source.forward[level] = new_node
     end
+  end
 
-    def search value
-        if (node_search_path(value).last.next.value == value)
-            value
-        end
+  def search value
+    if (node_search_path(value).last.next.value == value)
+      value
     end
+  end
 
-    def delete value
-        search_path = node_search_path value
-        node = search_path.last.next
-        if (node.value == value)
-            search_path.reverse[0,node.forward.size].each_with_index do |n, level|
-                n.forward[level] = node.forward[level]
-            end
-        end
-    end
-
-    def each
-      node = @head.next
-      until (node == @last)
-        yield node.value
-        node = node.next
+  def delete value
+    search_path = node_search_path value
+    node = search_path.last.next
+    if (node.value == value)
+      search_path.reverse[0,node.forward.size].each_with_index do |n, level|
+        n.forward[level] = node.forward[level]
       end
+    end
+  end
 
-      self
+  def each
+    node = @head.next
+    until (node == @last)
+      yield node.value
+      node = node.next
     end
 
-    def to_a
-      reduce([], :<<)
-    end
+    self
+  end
 
-    def to_s
-        str = ''
-        each_node do |node|
-            str << " #{node.value} "
+  def to_a
+    reduce([], :<<)
+  end
+
+  def to_s
+    str = ''
+    each_node do |node|
+      str << " #{node.value} "
+    end
+    str << "\n"
+    (0..max_level).each do |level|
+      each_node do |node|
+        o = node.forward[level]
+        if o
+          p = o.value.to_s
+        else
+          p = 'n'
         end
-        str << "\n"
-        (0..max_level).each do |level|
-            each_node do |node|
-                o = node.forward[level]
-                if o
-                    p = o.value.to_s
-                else
-                    p = 'n'
-                end
-                str << " #{p} "
-            end
-            str << "\n"
-        end
-        return str
+        str << " #{p} "
+      end
+      str << "\n"
     end
+    return str
+  end
 
-    def each_node
-        node = @head
-        until node.nil?
-            yield node
-            node = node.next
-        end
+  def each_node
+    node = @head
+    until node.nil?
+      yield node
+      node = node.next
     end
+  end
 
-    def node_search_path value
-        node = @head
-        max_level.downto(0).reduce([]) do |path, level|
-           path.tap {|path|
-             node = find_left_neighbor(level, node, value)
-             path << node
-           }
-        end
+  def node_search_path value
+    node = @head
+    max_level.downto(0).reduce([]) do |path, level|
+      path.tap {|path|
+        node = find_left_neighbor(level, node, value)
+        path << node
+      }
     end
+  end
 
-    def find_left_neighbor(level, node, value)
-        while (node.forward[level].value < value)
-            node = node.forward[level]
-        end
-        return node
+  def find_left_neighbor(level, node, value)
+    while (node.forward[level].value < value)
+      node = node.forward[level]
     end
+    return node
+  end
 
-    def random_width
-        h = 1
-        while (rand < 0.5 and h <= max_level)
-            h += 1
-        end
-        return h
+  def random_width
+    h = 1
+    while (rand < 0.5 and h <= max_level)
+      h += 1
     end
+    return h
+  end
 
-    def insert *elements
-        elements.each(&method(:<<))
-    end
+  def insert *elements
+    elements.each(&method(:<<))
+  end
 end
-
--- test/test_skiplist.rb
+++ test/test_skiplist.rb
@@ -4,36 +4,35 @@ require 'test/unit'
 require 'skiplist.rb'
 
 class TestSkipList < Test::Unit::TestCase
-    def setup
-        @unsorted = [84, 52, 53, 35, 46, 18, 95, 88, 26, 4]
-        @sorted   = @unsorted.sort
-        @skiplist = SkipList.new(16)
-        @skiplist.insert(*@unsorted)
-    end
+  def setup
+    @unsorted = [84, 52, 53, 35, 46, 18, 95, 88, 26, 4]
+    @sorted   = @unsorted.sort
+    @skiplist = SkipList.new(16)
+    @skiplist.insert(*@unsorted)
+  end
 
-    def test_insert_array
-         assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
-    end
+  def test_insert_array
+    assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
+  end
 
-    def test_insert_value_using_insert
-        @skiplist.insert(10)
-        assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
-    end
+  def test_insert_value_using_insert
+    @skiplist.insert(10)
+    assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
+  end
 
-    def test_insert_value_using_push
-        @skiplist << 10
-        assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
-    end
+  def test_insert_value_using_push
+    @skiplist << 10
+    assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
+  end
 
-    def test_search
-        assert_equal(52, @skiplist.search(52), 'Should return 52 as 52 is a skiplist member')
-        assert_equal(nil, @skiplist.search(54), 'Should return nil as 54 is not a skiplist member')
-    end
+  def test_search
+    assert_equal(52, @skiplist.search(52), 'Should return 52 as 52 is a skiplist member')
+    assert_equal(nil, @skiplist.search(54), 'Should return nil as 54 is not a skiplist member')
+  end
 
-    def test_delete
-        @skiplist.delete(52)
-        @sorted.delete(52)
-        assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
-    end
-
+  def test_delete
+    @skiplist.delete(52)
+    @sorted.delete(52)
+    assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
+  end
 end
