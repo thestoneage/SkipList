a6658f6387ce0c1753480bc8b3e4405e2a7de768
Jörg W Mittag <JoergWMittag+Ruby@GoogleMail.Com>
2010-08-03 12:13:56 +0200

    DRY up the tests somewhat.

--- test/test_skiplist.rb
+++ test/test_skiplist.rb
@@ -8,33 +8,29 @@ class TestSkipList < Test::Unit::TestCase
         @unsorted = [84, 52, 53, 35, 46, 18, 95, 88, 26, 4]
         @sorted   = @unsorted.sort
         @skiplist = SkipList.new(16)
+        @skiplist.insert(*@unsorted)
     end
 
     def test_insert_array
-         @skiplist.insert(*@unsorted)
          assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
     end
 
     def test_insert_value_using_insert
-        @skiplist.insert(*@unsorted)
         @skiplist.insert(10)
         assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
     end
 
     def test_insert_value_using_push
-        @skiplist.insert(*@unsorted)
         @skiplist << 10
         assert_equal((@unsorted << 10).sort, @skiplist.to_a, 'Should be equal')
     end
 
     def test_search
-        @skiplist.insert(*@unsorted)
         assert_equal(52, @skiplist.search(52), 'Should return 52 as 52 is a skiplist member')
         assert_equal(nil, @skiplist.search(54), 'Should return nil as 54 is not a skiplist member')
     end
 
     def test_delete
-        @skiplist.insert(*@unsorted)
         @skiplist.delete(52)
         @sorted.delete(52)
         assert_equal(@sorted, @skiplist.to_a, 'Should be equal')
